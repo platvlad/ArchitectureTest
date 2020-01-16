8422012a9de0ad40bda0d36390444554ea0f1816
Vladislav Platonov <v.platonov@phoenixcas.com>
2020-01-16 23:20:20 +0300

    Fix sort

--- server/src/main/java/architectureTest/server/Sorter.java
+++ server/src/main/java/architectureTest/server/Sorter.java
@@ -1,13 +1,10 @@
 package architectureTest.server;
 
-import java.util.Collections;
-import java.util.List;
-
 public class Sorter {
     static void sort(Long[] elems) {
         int numElems = elems.length;
-        for (int i = numElems - 1; numElems >= 0; numElems--) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = numElems - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 if (elems[j] > elems[i]) {
                     Long tmp = elems[i];
                     elems[i] = elems[j];
